56e5de72dc9b6a2a3d63eb69721d4fe0fc12fc2c
Boris <boris.marinov99@gmail.com>
2022-03-26 16:01:37 +0200

    add images, template and some components as progress

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
+// eslint-disable
+
 import { createApp } from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import './assets/tailwind.css'
+import './assets/main.css'
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+
+app.mount('#app');
